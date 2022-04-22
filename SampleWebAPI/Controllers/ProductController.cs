5ed17ac7e5dde0af4e584c5cf229d5c5adf76355
ajinkya hake <hakeajinkya2@gmail.com>
2022-04-22 12:51:38 +0530

    Add project files.

---
+++ SampleWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleWebAPI.Services;
+using SampleWebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductServices _prodservice;
+        private object prod;
+
+        public ProductController(IProductServices prodservice)
+        {
+            _prodservice = prodservice;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        [Route("api/Product/GetProducts")]
+
+        public IActionResult GetProduct(Product prod)
+        {
+            return new ObjectResult(_prodservice.AddProduct(prod));
+        }
+        [HttpGet]
+        [Route("DeleteProduct/{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            return new ObjectResult(_prodservice.DeleteProduct(id));
+        }
+    }
+}
---
+++ SampleWebAPI/Entities/IProductServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampleWebAPI.Entities
+{
+    public class IProductServices
+    {
+        internal object AddProduct(object prod)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
